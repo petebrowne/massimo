8c24e84e979b449ad6c966cb00a0c3e3b0ebfe58
Peter Browne <me@petebrowne.com>
2010-04-17 14:10:37 -0500

    Add Site#resources and Site#resource for adding resources to process

--- lib/massimo/site.rb
+++ lib/massimo/site.rb
@@ -2,10 +2,26 @@ module Massimo
   class Site
     attr_accessor :config
     
+    DEFAULT_RESOURCES = [ Massimo::Page ].freeze
+    
     def initialize(options = nil)
       @config = Config.new(options)
       yield @config if block_given?
       Massimo.site = self
+    end
+    
+    def resources
+      @resources ||= DEFAULT_RESOURCES.dup
+    end
+    
+    def resource(name_or_class, &block)
+      resource = case name_or_class
+        when Class
+          name_or_class
+        else
+          Object.const_set name_or_class.to_s.classify, Class.new(Massimo::Page, &block)
+        end
+      resources << resource
     end
   end
 end
--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -27,4 +27,51 @@ describe Massimo::Site do
       end
     end
   end
+  
+  describe '#resources' do
+    it 'should be an array' do
+      Massimo::Site.new.resources.should be_an_instance_of(Array)
+    end
+    
+    it 'should include Massimo::Page by default' do
+      Massimo::Site.new.resources.should include(Massimo::Page)
+    end
+  end
+  
+  describe '#resource' do
+    context 'with a Class constant' do
+      it "should add a resource to the site's resources" do
+        Post = Class.new(Massimo::Resource)
+        site = Massimo::Site.new
+        site.resource Post
+        site.resources.should include(Post)
+      end
+    end
+    
+    context 'with a Class body' do
+      before do
+        @site = Massimo::Site.new
+        @site.resource :comment do
+          def spam?
+            true
+          end
+        end
+      end
+      
+      it 'should create a class that inherits from Page' do
+        Comment.superclass.should == Massimo::Page
+      end
+      
+      it "should add the class to the site's resources" do
+        @site.resources.should include(Comment)
+      end
+      
+      it 'should add the methods in the class body' do
+        within_construct do |c|
+          c.file 'comment.txt'
+          Comment.new('comment.txt').should be_spam
+        end
+      end
+    end
+  end
 end
