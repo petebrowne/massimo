2b77bb4b5bce51a66b2c31ec2b391d670c49938b
Pete Browne <me@petebrowne.com>
2011-05-23 13:34:29 -0500

    Refactoring Massimo::View#render to use method built-in to Massimo::Resource when possible

--- lib/massimo/view.rb
+++ lib/massimo/view.rb
@@ -5,9 +5,9 @@ module Massimo
     unprocessable
     
     def render(locals = {}, &block)
-      options  = Massimo.config.options_for(source_path.extname[1..-1])
-      template = Tilt.new(source_path.to_s, 1, options) { content }
-      template.render(Massimo.site.template_scope, locals, &block)
+      template_options = Massimo.config.options_for(extension[1..-1])
+      template         = Tilt.new(source_path.to_s, 1, template_options) { content }
+      template.render(template_scope, locals, &block)
     end
   end
 end
