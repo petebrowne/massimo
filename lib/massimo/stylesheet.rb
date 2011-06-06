9db1192e1ee71726ad05bfa0f4c514a864d25574
Pete Browne <me@petebrowne.com>
2011-06-06 17:54:32 -0500

    Using Crush for compression of javascripts and stylesheets.

--- lib/massimo/stylesheet.rb
+++ lib/massimo/stylesheet.rb
@@ -1,3 +1,6 @@
+require 'crush'
+require 'tilt'
+
 module Massimo
   class Stylesheet < Massimo::Resource
     def extension
@@ -7,5 +10,19 @@ module Massimo
         super
       end
     end
+    
+    def render
+      compress(super)
+    end
+    
+    protected 
+  
+      def compress(stylesheet)
+        if engine_type = Crush.find_by_name(Massimo.config.css_compressor)
+          engine_type.new(source_path.to_s, Massimo.config.css_compressor_options) { stylesheet }.compress
+        else
+          stylesheet.strip
+        end
+      end
   end
 end
--- spec/massimo/stylesheet_spec.rb
+++ spec/massimo/stylesheet_spec.rb
@@ -44,7 +44,7 @@ describe Massimo::Stylesheet do
     it 'should render using Sass' do
       within_construct do |c|
         c.file 'stylesheets/main.sass', "#header\n  font-size: 36px"
-        stylesheet.render.should == "#header {\n  font-size: 36px; }\n"
+        stylesheet.render.should == "#header {\n  font-size: 36px; }"
       end
     end
     
@@ -52,7 +52,7 @@ describe Massimo::Stylesheet do
       within_construct do |c|
         c.file 'stylesheets/main.sass', '@import "base"'
         c.file 'stylesheets/_base.sass', "#header\n  font-size: 36px"
-        stylesheet.render.should == "#header {\n  font-size: 36px; }\n"
+        stylesheet.render.should == "#header {\n  font-size: 36px; }"
       end
     end
     
@@ -67,7 +67,7 @@ describe Massimo::Stylesheet do
       Massimo.config.sass = { :style => :compressed }
       within_construct do |c|
         c.file 'stylesheets/main.sass', "#header\n font-size: 36px"
-        stylesheet.render.should == "#header{font-size:36px}\n"
+        stylesheet.render.should == "#header{font-size:36px}"
       end
     end
   end
@@ -78,7 +78,7 @@ describe Massimo::Stylesheet do
     it 'should render using Sass' do
       within_construct do |c|
         c.file 'stylesheets/main.scss', "$size: 36px;\n#header { font-size: $size; }\n"
-        stylesheet.render.should == "#header {\n  font-size: 36px; }\n"
+        stylesheet.render.should == "#header {\n  font-size: 36px; }"
       end
     end
   end
@@ -89,7 +89,7 @@ describe Massimo::Stylesheet do
     it 'should render using Less' do
       within_construct do |c|
         c.file 'stylesheets/main.less', "@color: #000000;\n#header { color: @color; }"
-        stylesheet.render.should == "#header { color: #000000; }\n"
+        stylesheet.render.should == "#header { color: #000000; }"
       end
     end
     
@@ -97,6 +97,65 @@ describe Massimo::Stylesheet do
       within_construct do |c|
         c.file 'stylesheets/main.less'
         stylesheet.output_path.extname.should == '.css'
+      end
+    end
+  end
+  
+  context 'with compression' do
+    let(:stylesheet) { Massimo::Stylesheet.new 'stylesheets/main.css' }
+    let(:code)       { '#header { font-size: 36px }' }
+    
+    context 'using :cssmin' do
+      it 'compresses using CSSMin' do
+        Massimo.config.css_compressor = :cssmin
+        with_file 'stylesheets/main.css', code do
+          mock_module("CSSMin").minify(code) { '' }
+          stylesheet.render
+        end
+      end
+    end
+    
+    context 'using :rainpress' do
+      it 'compresses using Rainpress' do
+        Massimo.config.css_compressor = :rainpress
+        with_file 'stylesheets/main.css', code do
+          mock_module("Rainpress").compress(code, {}) { '' }
+          stylesheet.render
+        end
+      end
+      
+      context 'with configuration' do
+        it 'passes configuration to Rainpress' do
+          Massimo.config.css_compressor = :rainpress
+          Massimo.config.css_compressor_options = { :comments => false }
+          with_file 'stylesheets/main.css', code do
+            mock_module("Rainpress").compress(code, :comments => false) { '' }
+            stylesheet.render
+          end
+        end
+      end
+    end
+    
+    context 'using :yui_css' do
+      it 'compresses using YUI::CssCompressor' do
+        Massimo.config.css_compressor = :yui_css
+        with_file 'stylesheets/main.css', code do
+          compressor = mock!.compress(code) { '' }
+          mock_module("YUI::CssCompressor").new({}) { compressor }
+          stylesheet.render
+        end
+      end
+      
+      context 'with configuration' do
+        it 'passes configuration to YUI::CssCompressor' do
+          Massimo.config.css_compressor = :yui_css
+          Massimo.config.css_compressor_options = { :linebreak => 0 }
+          with_file 'stylesheets/main.css', code do
+            compressor = mock!.compress(code) { '' }
+            mock_module("YUI::CssCompressor").new(:linebreak => 0) { compressor }
+            stylesheet.render
+          end
+        end
       end
     end
   end
