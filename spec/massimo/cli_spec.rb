3cb49c10e99564dda0abf15c8b58c982077cdff4
Pete Browne <me@petebrowne.com>
2011-06-27 10:23:31 -0500

    Fixing expectation of config.rb template

--- spec/massimo/cli_spec.rb
+++ spec/massimo/cli_spec.rb
@@ -158,19 +158,16 @@ describe Massimo::CLI do
         content = <<-EOS.unindent
           require 'sass'
           require 'sprockets'
-
+          
           # This is an example configuration File
           # Look here for all the available options:
           # http://massimo.petebrowne.com/configuration/
-
+          
           if config.environment.production?
-            # Use JSMin for javascript compression
-            # config.javascripts_compressor = :min
-  
-            # Compress the output of Sass stylesheets
-            # config.sass = { :style => :compressed }
+            # Use Uglifier for javascript compression
+            # config.js_compressor = :uglifier
           end
-
+          
           helpers do
             # Define helper methods here
           end
